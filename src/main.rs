0d55091e110b3621a90d000551f65ec5579dff65
chansuke <chansuke@georepublic.de>
2018-08-16 00:45:49 +0900

    render function

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,26 @@ fn complex_square_add_loop(c: Complex<f64>) {
     }
 }
 
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }

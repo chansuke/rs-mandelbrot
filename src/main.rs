c4e20c44c5c460039016f7aa002fc99b538624fa
chansuke <chansuke@georepublic.de>
2018-08-16 00:24:17 +0900

    parse complex

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some((Complex { re, im })),
+        None => None,
+    }
+}
+
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
 
@@ -46,4 +53,15 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25, -0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
 }

500a3a2fb92029ebb75c4ac1c7afa0dec5b28e02
chansuke <chansuke@georepublic.de>
2018-08-16 22:09:30 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -45,30 +45,21 @@ fn test_parse_complex() {
 fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> Complex<f64> {
-    let (width, height) = (
-        lower_right.re - upper_left.re,
-        upper_left.im - lower_right.im,
-    );
-
-    Complex {
-        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
-    }
+    upper_left: (f64, f64),
+    lower_right: (f64, f64),
+) -> (f64, f64) {
+    let (width, height) = (lower_right.0 - upper_left.0, upper_left.1 - lower_right.1);
+    (
+        upper_left.0 + pixel.0 as f64 * width / bounds.0 as f64,
+        upper_left.1 - pixel.1 as f64 * height / bounds.1 as f64,
+    )
 }
 
 #[test]
 fn test_pixel_to_point() {
     assert_eq!(
-        pixel_to_point(
-            (100, 100),
-            (25, 75),
-            Complex { re: -1.0, im: 1.0 },
-            Complex { re: 1.0, im: -1.0 }
-        ),
-        Complex { re: -0.5, im: -0.5 }
+        pixel_to_point((100, 100), (25, 75), (-1.0, 1.0), (1.0, -1.0)),
+        (-0.5, -0.5)
     );
 }
 
@@ -96,16 +87,16 @@ fn complex_square_add_loop(c: Complex<f64>) {
 fn render(
     pixels: &mut [u8],
     bounds: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
+    upper_left: (f64, f64),
+    lower_right: (f64, f64),
 ) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for row in 0..bounds.1 {
-        for column in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+    for r in 0..bounds.1 {
+        for c in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (c, r), upper_left, lower_right);
 
-            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+            pixels[r * bounds.0 + c] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
             };
